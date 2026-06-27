cbca2884027862c43d1b5d0ba470119ef3289047
404xg31 <desimemer11@gmail.com>
2026-06-27 22:01:44 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_coful.mk
+    $(LOCAL_DIR)/fox_coful.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_coful-user \
-    twrp_coful-userdebug \
-    twrp_coful-eng
+    fox_coful-user \
+    fox_coful-userdebug \
+    fox_coful-eng
