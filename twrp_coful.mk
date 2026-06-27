041f76f72afb0d3f42615af4ca4558cd05aaba74
404xg31 <desimemer11@gmail.com>
2026-06-28 00:10:06 +0530

    Refactor product inheritance in twrp_coful.mk
    Removed inheritance from core and full base telephony products.

--- twrp_coful.mk
+++ twrp_coful.mk
@@ -13,10 +13,6 @@ DEVICE_PATH := device/motorola/coful
 ##Inherit​ any OrangeFox-specific settings
 $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from coful device
 $(call inherit-product, device/motorola/coful/device.mk)
 
