ca46a2a86cb78fc68b7193e63440f27322a22f1d
404xg31 <desimemer11@gmail.com>
2026-06-27 22:45:52 +0530

    Update TWRP configuration for coful device

--- twrp_coful.mk
+++ twrp_coful.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/orangefox/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from coful device
 $(call inherit-product, device/motorola/coful/device.mk)
 
 PRODUCT_DEVICE := coful
-PRODUCT_NAME := fox_coful
+PRODUCT_NAME := twrp_coful
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g31
 PRODUCT_MANUFACTURER := motorola
@@ -27,8 +27,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="coful_g-user 12 S3RWBS32.125-29-2-4-3 3c754f release-keys"
 
 BUILD_FINGERPRINT := motorola/coful_g/coful:12/S3RWBS32.125-29-2-4-3/3c754f:user/release-keys
-
-# OrangeFox specific variables
-FOX_VERSION := R12.1
-FOX_BUILD_DEVICE := coful
-
