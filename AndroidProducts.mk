856ffce66b4be1cbf5c02feea015e5caf07678d2
404xg31 <desimemer11@gmail.com>
2026-06-27 18:22:30 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fox_coful.mk
 
 COMMON_LUNCH_CHOICES := \
-    Omni_coful-user \
-    Omni_coful-userdebug \
-    Omni_coful-eng
+    fox_coful-user \
+    fox_coful-userdebug \
+    fox_coful-eng
