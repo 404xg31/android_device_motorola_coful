e466699f4a5d745973c085a2e671eb66d79cb8c6
404xg31 <desimemer11@gmail.com>
2026-06-27 14:01:56 +0530

    Rename product from fox_coful to omni_coful

--- Omni_coful.mk
+++ Omni_coful.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/orangefox/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from coful device
 $(call inherit-product, device/motorola/coful/device.mk)
 
 PRODUCT_DEVICE := coful
-PRODUCT_NAME := fox_coful
+PRODUCT_NAME := omni_coful
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g31
 PRODUCT_MANUFACTURER := motorola
