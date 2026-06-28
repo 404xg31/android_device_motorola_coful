03a26c661ae05130b2932c0476845492f9af9f7a
404xg31 <desimemer11@gmail.com>
2026-06-28 17:02:16 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,9 @@
-#
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
 PRODUCT_MAKEFILES := \
-    device/motorola/coful/twrp_coful.mk
+    $(LOCAL_DIR)/twrp_coful.mk
 
 COMMON_LUNCH_CHOICES := \
     twrp_coful-user \
