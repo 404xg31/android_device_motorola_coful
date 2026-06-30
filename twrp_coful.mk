2681063575c202feaf3ef38f888aa37f2e8e58c8
404xg31 <desimemer11@gmail.com>
2026-06-30 17:40:15 +0530

    Update twrp_coful.mk

--- twrp_coful.mk
+++ twrp_coful.mk
@@ -2,7 +2,7 @@ orangefoxefoxyright (C) 2026 The Android Open Source Project
 # SPDX-License-Identifier: Apache-2.0
 
 # Inherit OrangeFox configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/orangefox/config/common.mk)
 
 # Inherit device setup
 $(call inherit-product, device/motorola/coful/device.mk)
