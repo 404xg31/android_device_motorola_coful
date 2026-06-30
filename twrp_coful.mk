f82e8f240e154b63414125fbea1648e34e7f39dc
404xg31 <desimemer11@gmail.com>
2026-06-30 18:02:43 +0530

    Fix DEVICE_PATH typo in twrp_coful.mk

--- twrp_coful.mk
+++ twrp_coful.mk
@@ -5,7 +5,7 @@
 PRODUCT_RELEASE_NAME := coful
 
 ##Device​ path for OEM device tree
-DEVICE_PATH := devie/motorola/coful
+DEVICE_PATH := device/motorola/coful
 
 ##Inherit​ any OrangeFox-specific settings
 $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
